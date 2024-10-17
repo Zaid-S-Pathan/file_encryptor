d104d12a0720c67a04af35c60f7cb22c1755c65f
Zaid-S-Pathan <mohammedzaid.s.pathan@gmail.com>
2024-10-18 03:06:08 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_encryption_package',
+    name='file_encryptor',
     version='0.1',
     packages=find_packages(),
     install_requires=['cryptography'],  # Dependencies
     author='Your Name',
     author_email='your.email@example.com',
     description='A file encryption package',
-    url='https://github.com/yourusername/my_encryption_package',  # Optional GitHub URL
+    url='https://github.com/Zaid-S-Pathan/file_encryptor',  # Optional GitHub URL
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
