ec1826fc1d889f4318027ca6e6732c726d509dc5
Zaid-S-Pathan <mohammedzaid.s.pathan@gmail.com>
2024-10-21 23:56:33 +0530

    Updated encryption logic or fixed bugs

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='file_encryptor_atharva_project_19',
+    name='file_encryptor_atharva_project_14',
     version='0.1',
     packages=find_packages(),
     install_requires=['cryptography'],
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Zaid Pathan',
+    author_email='zaidspathan@gmail.com',
     description='A file encryption package',
     url='https://github.com/Zaid-S-Pathan/file_encryptor', 
     classifiers=[
