8090b1db8539e9958596c1f675de950215b4539f
Zaid-S-Pathan <mohammedzaid.s.pathan@gmail.com>
2024-10-21 22:07:00 +0530

    Initial commit: added encryption package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='file_encryptor',
+    name='file_encryptor_atharva_project_19',
     version='0.1',
     packages=find_packages(),
-    install_requires=['cryptography'],  # Dependencies
+    install_requires=['cryptography'],
     author='Your Name',
     author_email='your.email@example.com',
     description='A file encryption package',
-    url='https://github.com/Zaid-S-Pathan/file_encryptor',  # Optional GitHub URL
+    url='https://github.com/Zaid-S-Pathan/file_encryptor', 
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
